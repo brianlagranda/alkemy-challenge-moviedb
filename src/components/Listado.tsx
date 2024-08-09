ac0aeffcc698c7345cd8a69fde22faed792b2181
Brian Lagranda <brianlagranda@gmail.com>
2024-08-09 17:38:04 -0300

    feat: Add list of movies by popularity and es-ES language.

--- src/components/Listado.tsx
+++ src/components/Listado.tsx
@@ -1,7 +1,72 @@
-function Listado() {
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Link, Navigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+
+interface Movie {
+    id: string;
+    title: string;
+    overview: string;
+    poster_path: string;
+}
+
+const MySwal = withReactContent(Swal);
+
+const Listado = () => {
+    const [moviesList, setMoviesList] = useState<Movie[]>([]);
+
     const Token = localStorage.getItem('token');
 
-    return Token && <div>Listado de películas</div>;
-}
+    useEffect(() => {
+        axios
+            .get(
+                'https://api.themoviedb.org/3/discover/movie?language=es-ES&page=1&api_key=a38828a7abe4db65150dacdb2df29843',
+            )
+            .then((res) => {
+                setMoviesList(res.data.results);
+            })
+            .catch((e) => {
+                MySwal.fire({
+                    title: 'Hubo un error inesperado, intente nuevamente más tarde',
+                    icon: 'error',
+                });
+                console.log(`Error: ${e}`);
+            });
+    }, [setMoviesList]);
+
+    return (
+        <>
+            {!Token && <Navigate to="/login" />}
+
+            <div className="grid grid-cols-1 gap-4">
+                {moviesList.map((movie, idx) => {
+                    return (
+                        <div
+                            key={idx}
+                            className="flex w-full flex-col rounded border"
+                        >
+                            <img
+                                src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                            ></img>
+                            <div className="flex h-full w-full flex-col gap-2 p-2">
+                                <h2 className="text-xl font-bold">
+                                    {movie.title}
+                                </h2>
+                                <p className="">{`${movie.overview.substring(0, 80)}...`}</p>
+                                <Link
+                                    to={`/detalle?movieID=${movie.id}`}
+                                    className="w-1/2 rounded border bg-black p-2 text-center text-white hover:bg-white hover:text-black"
+                                >
+                                    View detail
+                                </Link>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+        </>
+    );
+};
 
 export default Listado;
