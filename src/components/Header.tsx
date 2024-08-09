aa9c70a03ebef93cdb6e9ed512ef0c41dd9c080b
Brian Lagranda <brianlagranda@gmail.com>
2024-08-09 17:37:10 -0300

    chore: Fix logo redirection

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,9 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <header className="flex h-12 items-center justify-between bg-black p-4 text-white">
-            <h1 className="text-xl font-bold">AlkeFlix</h1>
+            <Link to="/" className="text-xl font-bold">
+                AlkeFlix
+            </Link>
             <nav className="">
                 <ul className="flex gap-2">
                     <li className="hover:bg-white hover:text-black">
